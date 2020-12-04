6a13d24e2eacf2b4b2a9797db8df2ced1d5d53f0
Bequem <1thebeck@gmail.com>
2020-12-04 00:42:33 -0300

    crud commit

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>B-DEV Vagas!!</title>
+  </head>
+  <body class="bg-dark text-light">
+
+    <div class="container">
+
+      <div class="jumbotron bg-warning">
+        <h1>B-DEV VAGAS</h1>
+        <p>CRUD com PHP Orientado a Objetos & MySQL. </p>
+      </div>
+    
+
